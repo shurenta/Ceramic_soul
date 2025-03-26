0a2ca7d54caf6c0e6d0e9e371645f6b8ea65965f
aleksandr <drevs.aleksandr@gmail.com>
2025-03-26 16:37:56 +0300

    finish index

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,36 @@
+import "purecss/build/grids-min.css";
+import "purecss/build/grids-responsive-min.css";
+
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "/src/sass/style.scss";
 
+try {
+    const swiper = new Swiper('.works__slider', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+        navigation: {
+            nextEl: ".icon-right-open",
+            prevEl: ".icon-left-open"
+        },
+        breakpoints: {
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5
+            },
+            1920: {
+                spaceBetween: 35
+            }
+        },
+        modules: [Navigation, Pagination],
+    });
+} catch (e) { }
