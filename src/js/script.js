be825c0e96184cd3e1be668c6d3a71c05cdca18c
aleksandr <drevs.aleksandr@gmail.com>
2025-04-09 15:06:41 +0300

    swiper

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,10 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import "/src/sass/style.scss";
+import '/swiper/css';
+import '/swiper/css/navigation';
+import '/swiper/css/pagination';
+import '/src/sass/style.scss';
 
 const burger = document.querySelector(".burger"),
     close = document.querySelector(".header__menu-close"),
