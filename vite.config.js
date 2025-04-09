b2ad2a59c23bee2c9c4066cfc7ffeaca9f3ea2be
aleksandr <drevs.aleksandr@gmail.com>
2025-04-09 14:23:36 +0300

    add about page

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
                 main: resolve(__dirname, 'index.html'),
                 catalog: resolve(__dirname, 'catalog.html'),
                 blog: resolve(__dirname, 'blog.html'),
+                about: resolve(__dirname, 'about.html')
             },
         },
     },
